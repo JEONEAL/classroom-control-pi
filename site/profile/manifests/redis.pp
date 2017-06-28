f488cc5655780c9e3e024417ba865bae91a77e3b
JEONEAL <john_oneal@xtoenergy.com>
2017-06-28 12:30:34 -0500

    Update redis.pp

--- site/profile/manifests/redis.pp
+++ site/profile/manifests/redis.pp
@@ -5,8 +5,8 @@ class profile::redis {
   
   class { 'redis':
     # what parameter should we pass to set maxmemory to 10mb?
-    
+    maxmemory => '10mb
   }
   contain redis
 }
-maxmemory => '10mb
+
